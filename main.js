12279e3a9e70a552fc3b9ecae521f96d6215e49b
Ryan Chung <ryan.jone@gmail.com>
2018-09-02 23:45:48 +0800

    even row different css

--- main.js
+++ main.js
@@ -29,11 +29,22 @@ function setTable(){
     
     var topicCount = topic.length;
     for(var x=0;x<topicCount;x++){
-        $("#courseTable").append("<tr>");
-        $("#courseTable").append("<td>"+(x+1)+"</td>");
-        $("#courseTable").append("<td>"+(new Date(startDate.getTime()+7*x*24*60*60*1000)).toLocaleDateString()+"</td>");
-        $("#courseTable").append("<td>"+topic[x]+"</td>");
-        $("#courseTable").append("</tr>");
+        if(x%2==0)
+        {
+            $("#courseTable").append("<tr>");
+            $("#courseTable").append("<td class='even'>"+(x+1)+"</td>");
+            $("#courseTable").append("<td class='even'>"+(new Date(startDate.getTime()+7*x*24*60*60*1000)).toLocaleDateString()+"</td>");
+            $("#courseTable").append("<td class='even'>"+topic[x]+"</td>");
+            $("#courseTable").append("</tr>"); 
+        }else
+        {
+            $("#courseTable").append("<tr>");
+            $("#courseTable").append("<td>"+(x+1)+"</td>");
+            $("#courseTable").append("<td>"+(new Date(startDate.getTime()+7*x*24*60*60*1000)).toLocaleDateString()+"</td>");
+            $("#courseTable").append("<td>"+topic[x]+"</td>");
+            $("#courseTable").append("</tr>");
+        }
+        
     }
       
 }
