21ee278e2e1a99ca9eda24255c4944d9629afb38
Ryan Chung <ryan.jone@gmail.com>
2018-09-02 23:32:28 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    $("#courseTable").append("<tr>");
+    $("#courseTable").append("<th>場次</th>");
+    $("#courseTable").append("<th>時間</th>");
+    $("#courseTable").append("<th>主題</th>");
+    $("#courseTable").append("</tr>");
+    
+    $( "input[type=date]" ).change(function() {
+        //alert( $( "input[type=date]" ).val() );
+        //2018-09-03
+        var inputDate = $( "input[type=date]" ).val();
+        var splitText = inputDate.split("-");
+        console.log(splitText[1]);
+        setMonthAndDay(splitText[1],splitText[2]);
+        setTable();
+    });
+     
+});
+
+function setTable(){
+    
+    $("#courseTable").empty();
+    
+    $("#courseTable").append("<tr>");
+    $("#courseTable").append("<th>場次</th>");
+    $("#courseTable").append("<th>時間</th>");
+    $("#courseTable").append("<th>主題</th>");
+    $("#courseTable").append("</tr>");
+    
+    var topicCount = topic.length;
+    for(var x=0;x<topicCount;x++){
+        $("#courseTable").append("<tr>");
+        $("#courseTable").append("<td>"+(x+1)+"</td>");
+        $("#courseTable").append("<td>"+(new Date(startDate.getTime()+7*x*24*60*60*1000)).toLocaleDateString()+"</td>");
+        $("#courseTable").append("<td>"+topic[x]+"</td>");
+        $("#courseTable").append("</tr>");
+    }
+      
+}
+
